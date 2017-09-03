9e8ef3f7403f319bda7ae20c74f0a4547e1cc871
lucas.martins <lucas.martins@totvs.com.br>
2017-09-03 13:36:47 -0300

    created basic trace with zipkin and opentracing-go

---
+++ test/zipkin/zipkin_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+  "context"
+  "testing"
+  "log"
+
+  "github.com/lucasmbaia/grpc-base/zipkin"
+)
+
+func TestNewCollector(t *testing.T) {
+  var (
+    err	error
+    collector zipkin.Collector
+  )
+
+  if collector, err = zipkin.NewCollector("http://172.16.95.113:9411/api/v1/spans", "127.0.0.1:0", "base", false); err != nil {
+    log.Fatalf("Erro to create new collector: ", err)
+  }
+
+  log.Println(collector)
+}
+
+func TestFlowZipkin(t *testing.T) {
+  var (
+    collector zipkin.Collector
+    err	      error
+    tags      = make(map[string]string)
+    ctx	      = context.Background()
+  )
+
+  if collector, err = newCollector("shit"); err != nil {
+    log.Fatalf("Error to create new collector: ", err)
+  }
+
+  s := collector.OpenFatherSpan(ctx, "Start", tags, nil)
+
+  collector2, _ := newCollector("god")
+  s.Event([]string{"Pocs"})
+  s2 := collector.OpenChildSpan(s.Ctx, "Pocs", tags, nil)
+  sc := collector.OpenChildSpan(s2.Ctx, "cururu", tags, s2.Span)
+
+  sc.Span.Finish()
+  s2.Span.Finish()
+  collector2.Close()
+
+  collector3, _ := newCollector("war")
+  s.Event([]string{"Coco"})
+  s3 := collector.OpenChildSpan(s.Ctx, "Coco", tags, nil)
+  sc2 := collector.OpenChildSpan(s3.Ctx, "pega", tags, nil)
+
+  sc2.Span.Finish()
+  s3.Span.Finish()
+  collector3.Close()
+
+  s.Span.Finish()
+  collector.Close()
+}
+
+func newCollector(name string) (zipkin.Collector, error) {
+  return zipkin.NewCollector("http://172.16.95.113:9411/api/v1/spans", "0.0.0.0:0", name, true)
+}
+
+/*func TestPorra(t *testing.T) {
+  var (
+    tags  = make(map[string]string)
+  )
+
+  //no fromhttprequest manda o god
+  shit, _ := newCollector("shit")
+  god, _ := newCollector("god")
+
+  shitSpan := shit.OpenFatherSpan(context.Background(), "Start", tags, nil)
+  shitSpan.Span.LogEvent("Call God")
+
+  s, ctx := opentracing.StartSpanFromContext(shitSpan.Ctx, "God")
+
+  spanMidleware := opentracing.SpanFromContext(ctx)
+
+  spanMidleware.SetTag("Tamo", "Junto")
+
+  req, _ := http.NewRequest("GET", "http://myservice/", nil)
+
+  if err := shit.Tracer.Inject(
+    spanMidleware.Context(),
+    opentracing.TextMap,
+    opentracing.HTTPHeadersCarrier(req.Header),
+  ); err != nil {
+    log.Println(err)
+  }
+
+  wireContext, _ := shit.Tracer.Extract(
+    opentracing.TextMap,
+    opentracing.HTTPHeadersCarrier(req.Header),
+  )
+
+  spanFrom := god.Tracer.StartSpan("God", ext.RPCServerOption(wireContext))
+  opentracing.ContextWithSpan(ctx, spanFrom)
+
+  spanFrom.Finish()
+  spanMidleware.Finish()
+  s.Finish()
+  shitSpan.Span.Finish()
+  god.Close()
+  shit.Close()
+}*/
+
--- zipkin/zipkin.go
+++ zipkin/zipkin.go
@@ -1,15 +1,20 @@
 package zipkin
 
 import (
-  "log"
+  "context"
 
   opentracing "github.com/opentracing/opentracing-go"
-  zipkin "github.com/openzipkin/zipkin-go-opentracing"
+  zipkinTracer "github.com/openzipkin/zipkin-go-opentracing"
 )
 
 type Collector struct {
-  Conn	  zipkin.Collector
+  Conn	  zipkinTracer.Collector
   Tracer  opentracing.Tracer
+}
+
+type Span struct {
+  Span	opentracing.Span
+  Ctx	context.Context
 }
 
 func NewCollector(url, hostPort, endPoint string, debug bool) (Collector, error) {
@@ -18,15 +23,67 @@ func NewCollector(url, hostPort, endPoint string, debug bool) (Collector, error)
     err	      error
   )
 
-  if collector.Conn, err = zipkin.NewHTTPCollector(url); err != nil {
-    return collector, nil
+  if collector.Conn, err = zipkinTracer.NewHTTPCollector(url); err != nil {
+    return collector, err
   }
 
-  if collector.Tracer, err = zipkin.NewTracer(zipkin.NewRecorder(collector.Conn, debug, hostPort, endPoint)); err != nil {
-    return collector, nil
+  if collector.Tracer, err = zipkinTracer.NewTracer(
+    zipkinTracer.NewRecorder(collector.Conn, debug, hostPort, endPoint),
+    zipkinTracer.ClientServerSameSpan(true),
+    zipkinTracer.TraceID128Bit(true),
+  ); err != nil {
+    return collector, err
   }
 
-  apply := &StartSpanOptions{Tags: map[string]interface{}{"teste":"teste"}}
-  log.Println(collector.Tracer.StartSpan("Parent", opentracing.StartSpanOption{opentracing.Apply(apply)}))
-    return collector, nil
+  opentracing.SetGlobalTracer(collector.Tracer)
+
+  return collector, nil
+}
+
+func (c Collector) Close() {
+  c.Conn.Close()
+}
+
+func (c Collector) OpenFatherSpan(ctx context.Context, name string, tags map[string]string, parent opentracing.Span) Span {
+  var (
+    span	Span
+  )
+
+  if parent == nil {
+    span.Span = c.Tracer.StartSpan(name)
+  } else {
+    span.Span = c.Tracer.StartSpan(name, opentracing.ChildOf(parent.Context()))
   }
+
+  span.Ctx = opentracing.ContextWithSpan(ctx, span.Span)
+
+  for key, value := range tags {
+    span.Span.SetTag(key, value)
+  }
+
+  return span
+}
+
+func (c Collector) OpenChildSpan(ctx context.Context, name string, tags map[string]string, parent opentracing.Span) Span {
+  var (
+    span	Span
+  )
+
+  if parent == nil {
+    span.Span, span.Ctx = opentracing.StartSpanFromContext(ctx, name)
+  } else {
+    span.Span, span.Ctx = opentracing.StartSpanFromContext(ctx, name, opentracing.ChildOf(parent.Context()))
+  }
+
+  for key, value := range tags {
+    span.Span.SetTag(key, value)
+  }
+
+  return span
+}
+
+func (s Span) Event(event []string) {
+  for _, value := range event {
+    s.Span.LogEvent(value)
+  }
+}
