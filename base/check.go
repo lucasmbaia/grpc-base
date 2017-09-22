fc49247d6d6f3c52e5ee7ee82154e3f4a0d79481
lucas.martins <lucas.martins@totvs.com.br>
2017-09-22 11:01:54 -0300

    onlyCheck

--- base/check.go
+++ base/check.go
@@ -1,12 +1,22 @@
 package base
 
 import (
+  "strings"
   "net/http"
   "github.com/lucasmbaia/grpc-base/config"
 )
 
 func onlyCheck() error {
-  mux := http.NewServeMux()
-  mux.HandleFunc(config.EnvConfig.EndPointCheck, func(w http.ResponseWriter, r *http.Request){})
+  var (
+    mux	  *http.ServeMux
+    url	  = config.EnvConfig.EndPointCheck
+  )
+
+  if strings.Split(url, "/")[0] != "" {
+    url = "/" + url
+  }
+
+  mux = http.NewServeMux()
+  mux.HandleFunc(url, func(w http.ResponseWriter, r *http.Request){})
   return http.ListenAndServe(":" + config.EnvConfig.PortUrlCheck, mux)
 }
